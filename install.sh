4729adf7aacc64046b8e8939bfd5936b5102e27b
lehmancurtis147 <lehmancurtis147@gmail.com>
2019-06-09 06:00:33 -0500

    Debugged issue with group creation and tested with success

--- install.sh
+++ install.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 #
-################################
-##                            ##
-## Install android-udev-rules ##
-##                            ##
-################################
+clear
+## Colors
+BLUE='\e[34m'
+PURPLE='\e[35m'
+CYAN='\e[36m'
+WHITE='\e[37m'
+NC='\e[0m'
+#
+function confirm(){
+    read -p "$*"
+}
+#
+echo ""
+echo -e "    ${PURPLE} _________      ___              __           _     __ "
+echo -e "    / ____<  /     /   |  ____  ____/ /________  (_)___/ / ${NC}"
+echo -e "  ${BLUE} /___ \ / /_____/ /| | / __ \/ __  / ___/ __ \/ / __  / "
+echo -e "  ____/ // /_____/ ___ |/ / / / /_/ / /  / /_/ / / /_/ / ${NC}"
+echo -e "${CYAN} /_____//_/     /_/  |_/_/ /_/\__,_/_/   \____/_/\__,_/ ${CYAN}"
+echo "                                                BY: lehmancurtis147"
+wait
+echo ""
+echo ""
+confirm 'press [enter] if you wish to proceed, or ctrl+c to exit'
 #
 ## Install adb and fastboot
 sudo apt-get install android-tools-adb android-tools-fastboot
@@ -16,33 +34,27 @@ sudo apt-get install android-tools-adb android-tools-fastboot
 ## Install rules
 if [ ! /usr/lib/sysusers.d/android-udev.conf ]; then
     sudo cp ./android-udev.conf /usr/lib/sysusers.d
-else
-    echo "android-udev.conf already exist"
 fi
 
 if [ ! /etc/udev/rules.d/51-andriod.rules ]; then
     sudo cp -v ./51-android.rules /etc/udev/rules.d;
     sudo chmod a+r /etc/udev/rules.d/51-android.rules
-else
-    echo "rules are already installed"
 fi
 
 ## Set up adb users group
 #
 # Delete old rules
-sudo groupdel adbusers
+sudo groupdel adbusers; sudo addgroup adbusers
 
 # Configure adbusers
 if [ ! /usr/lib/sysusers.d/ ]; then
     sudo mkdir -p /usr/lib/sysusers.d/ && sudo cp android-udev.conf /usr/lib/sysusers.d/
-else
-    echo "adbusers already configured"
 fi
 
 sudo systemd-sysusers # (1)
 
 # Add user to adb group
-sudo usermod -a -G adbusers $(whoami)
+sudo usermod -aG adbusers $(whoami)
 
 # Reload rules
 sudo udevadm control --reload-rules
